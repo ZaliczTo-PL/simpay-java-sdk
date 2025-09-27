4b567aff1433d92df031eafa7a6ef6418e1c14ee
nightowl-devs <me@nightowl.dev>
2025-09-27 13:13:11 +0200

    fix: fix not imported class

--- src/main/java/pl/zaliczto/simpay/responses/payment/PaymentIpnNotification.java
+++ src/main/java/pl/zaliczto/simpay/responses/payment/PaymentIpnNotification.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import pl.zaliczto.simpay.enums.payment.IpnNotificationType;
 
@@ -23,11 +24,10 @@ public class PaymentIpnNotification {
     @JsonProperty("notification_id")
     private String notificationId;
     private Instant date; // ISO 8601
-    private JsonNode data; // raw data object - user can map based on type
-    private Object typedData; // mapped object (one of ipn.* classes) if deserialized with custom deserializer
+    private JsonNode data;
+    private Object typedData;
     private String signature;
 
-    // collect any other top level fields (forward compatibility)
     private final Map<String,Object> others = new LinkedHashMap<>();
     @JsonAnySetter
     public void any(String k, Object v) {
